dadfbaf730545649f9335615a65ed227b81d59b2
Leonardo D. Macedo <49958549+Ldm2003@users.noreply.github.com>
2021-05-15 21:22:43 -0300

    Delete a directory

--- a/javascript/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems);
-    var instance = M.Carousel.init({
-        fullWidth: true,
-        indicators: true
-    });
-});
